a960a4e91c9f1ac96a842fb451e377de75ae790f
martaadom <marta.dominguezm@alumnos.upm.es>
2019-10-25 10:12:28 +0200

    despues de la tutoria

--- app/components/Game.js
+++ app/components/Game.js
@@ -3,7 +3,7 @@ import Actionbar from './Actionbar';
 import Author from './Author';
 import Content from "./Content";
 
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet,TouchableHighlight} from 'react-native';
 import MyButton from "./MyButton";
 
 export default class Game extends React.Component {
@@ -26,21 +26,22 @@ export default class Game extends React.Component {
                              onChangeQuestion={this.props.onChangeQuestion}
                              onSubmit={this.props.onSubmit}
                              questions={this.props.questions}
-                               lengthQuestions={this.props.lengthQuestions}/>
+                               lengthQuestions={this.props.lengthQuestions}
+                            onReset={this.props.onReset}/>
                     <Author question={this.props.question} />
                 </View>
             );
 
         } else {
-            return(
-                <View>
+            return [
+                <View style={{flex:1}}>
                   <Text style={styles.congrats}>Congratulations, you have finished the quiz!!!!!</Text>
                   <Text style={styles.score}>SCORE: {this.props.score}</Text>
                     <MyButton
                               name="Reset"
                               onReset={this.props.onReset}/>
-                 </View>
-              );
+                 </View>,
+                <View style={{flex:2}}></View>];
         }
     }
 
@@ -49,10 +50,12 @@ export default class Game extends React.Component {
 const styles = StyleSheet.create({
     congrats:{
         textAlign:'center',
-        fontSize: 25
+        fontSize: 25,
+        flex: 1
     },
     score:{
         textAlign:'center',
-        fontSize: 38
+        fontSize: 38,
+        flex: 1
     }
 })
